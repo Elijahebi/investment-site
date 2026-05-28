1035245605b192ef8b0700078aa4339b1ca0bc98
Elijah Ebi <ebigideelijah@gmail.com>
2026-05-28 12:46:25 +0100

    Fix: Simplify serverless routing - use standard api/index.js pattern

--- api/[...route].js
+++
@@ -1,9 +0,0 @@
-/**
- * Vercel API Route Handler
- * Catches all API requests and forwards to Express backend
- */
-
-const app = require('../backend/server');
-
-// Export as a serverless function handler
-module.exports = app;
---
+++ api/index.js
@@ -0,0 +1,6 @@
+/**
+ * Vercel Serverless API Handler
+ * Main entry point for all /api requests
+ */
+
+module.exports = require('../backend/server');
